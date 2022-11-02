0bd2ae1401c7e345be0112775ffea2c1c950fbd0
Khalidpathan <48081505+Khalidpathan@users.noreply.github.com>
2022-11-02 12:47:12 +0530

    commit3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
+import { style } from '@angular/animations';
+import { NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import {MenuItem, PrimeIcons} from 'primeng/api';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'prime-srzktask';
+
+    title = 'srzktask';
+
+    kp: MenuItem[] = [];
+
+
+
+    items2: MenuItem[] = [];
+    value2: number = 1.00;
+    countries: any[] = [];
+  selectedCity1: any;
+
+  selectedCity2: any;
+
+
+    ngOnInit() {
+        this.kp = [
+            {
+                label: 'Personal',
+            },
+            {
+                label: 'Business',
+            }
+        ];
+
+
+        this.items2 = [
+            { label: 'Convert', icon: 'pi pi-fw pi-dollar'},
+            { label: 'Send', icon: 'pi pi-fw pi-send'},
+            { label: 'Charts', icon: 'pi pi-chart-line'},
+            { label: 'Alerts', icon: 'pi pi-bell'}
+        ];
+    }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MenubarModule} from 'primeng/menubar';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {InputNumberModule} from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
+import {CascadeSelectModule} from 'primeng/cascadeselect';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    ButtonModule,
+    CardModule,
+    TabMenuModule,
+    InputNumberModule,
+    FormsModule,
+    CascadeSelectModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
